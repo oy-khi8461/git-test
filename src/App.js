e0b75d43986a500ab52d0b885026a78b4e494213
khi8461 <hyunwoo.kwon2@oliveyoung.co.kr>
2023-02-03 11:11:47 +0900

    1삽입

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import useHook from "./customHook";
 function App() {
   const { test } = useHook();
 
-  console.log("App---", test);
+  console.log("App--1-", test);
 
   return (
     <div className="App">
