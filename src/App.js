1f41e4bb94ad5ec0321db8ae6c95c6dd514e0062
oy-khi8461 <hyunwoo.kwon2@oliveyoung.co.kr>
2023-02-05 22:41:14 +0900

    main: app 2로변경

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import useHook from "./customHook";
 function App() {
   const { test } = useHook();
 
-  console.log("App--2-", test);
-  console.log("App--3-", test);
+  console.log("App--1-", test);
 
   return (
     <div className="App">
