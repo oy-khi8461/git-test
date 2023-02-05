34217cf45beb5afb1d74733eb2c338c8a87d8065
oy-khi8461 <hyunwoo.kwon2@oliveyoung.co.kr>
2023-02-05 22:33:01 +0900

    test

--- src/component/testComponent.jsx
+++ src/component/testComponent.jsx
@@ -4,11 +4,7 @@ import useHook from "../customHook";
 const TestComponent = () => {
   const { test, setTest } = useHook();
 
-<<<<<<< HEAD
   console.log("test component--1-", test);
-=======
-  console.log("test component--2-", test);
->>>>>>> f4babd1 (테스트 컴포넌트2로변경)
 
   return (
     <button
